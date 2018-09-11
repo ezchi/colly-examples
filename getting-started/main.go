19cdad6ea8270bc9a6c5750f44d794fd9bf006df
Enze Chi <enze.chi@gmail.com>
2018-09-11 14:07:48 +1000

    Add getting-started example

---
+++ getting-started/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+	// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+	// colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		// c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	c.Visit("https://www.google.com")
+}
